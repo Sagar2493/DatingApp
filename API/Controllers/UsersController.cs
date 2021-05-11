d86e2303c81c5c5583132fd70f8b71e6d952ef0e
yourusername <sagar.bzn@outlook.com>
2021-05-11 15:46:41 +0530

    Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+           return await _context.Users.ToListAsync();
+       
+        }
+        //api/Users/3
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUserById(int id)
+        {
+           return _context.Users.Find(id); 
+        }
+    }
+}
